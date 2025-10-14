1c43799c40072197bc5ef4f3a38f47a2074028ce
Dieuveille_Matsios <matsionadieuveille@gmail.com>
2025-10-14 22:28:53 +0100

    Creation of the files <<ContentServices>> for the services: batiment and industrie

---
+++ src/contents/ContentServices.js
@@ -0,0 +1,86 @@
+// Import des icones
+
+// Import des images
+
+// Services
+export const services = {
+  //   ###############################################
+  // --------------------Bâtiment---------------------
+  //   ###############################################
+
+  batiment: [
+    {
+      image: "",
+      icone: "",
+      title: "Peinture",
+      description: "",
+    },
+    {
+      image: "",
+      icone: "",
+      title: "Froid",
+      description: "",
+    },
+    {
+      image: "",
+      icone: "",
+      title: "Elecctricité",
+      description: "",
+    },
+    {
+      image: "",
+      icone: "",
+      title: "Plomberie",
+      description: "",
+    },
+  ],
+
+  //   ###############################################
+  // --------------------Services---------------------
+  //   ###############################################
+
+  industrie: [
+    {
+      image: "",
+      title: "Peinture",
+      icone: "",
+      description: "",
+    },
+    {
+      image: "",
+      title: "Sablage",
+      icone: "",
+      description: "",
+    },
+    {
+      image: "",
+      title: "Instrumentation",
+      icone: "",
+      description: "",
+    },
+    {
+      image: "",
+      title: "Soudure",
+      icone: "",
+      description: "",
+    },
+    {
+      image: "",
+      title: "Tuyauterie",
+      icone: "",
+      description: "",
+    },
+    {
+      image: "",
+      title: "Calorifuge",
+      icone: "",
+      description: "",
+    },
+    {
+      image: "",
+      title: "Travaux en hauter (cordiste)",
+      icone: "",
+      description: "",
+    },
+  ],
+};
