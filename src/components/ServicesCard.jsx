9323d613986442bf25f1c93eebadfa8087b58950
Dieuveille_Matsios <matsionadieuveille@gmail.com>
2025-10-15 20:01:53 +0100

    Mise en place du composant Card_Service C'est un composant qui recupère les image, titres et description des services proposé par l'entreprise

---
+++ src/components/ServicesCard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const ServicesCard = ({ title, image, description }) => {
+  return (
+    <div className=" w-fit h-6/12 bg-white m-3 rounded-2xl overflow-hidden ">
+      <div className="flex flex-1 flex-col">
+        {/* ------------------------------------------------ */}
+        {/* ######### Image */}
+        {/* ------------------------------------------------ */}
+
+        <img
+          src={image}
+          alt={title}
+          className="w-[40rem] h-full opacity-[0.9]"
+        />
+
+        {/* ------------------------------------------------ */}
+        {/* ######### Part title & Description */}
+        {/* ------------------------------------------------ */}
+
+        <div className="p-3 flex-1 flex-col">
+          <p className="text-green-700 text-3xl font-bold text-start">
+            {title}
+          </p>
+          <p className=" overflow-hidden text-gray-800 text-justify text-2xl w-[20rem]">
+            {description}
+          </p>
+          <p className=" text-end text-xl font-bold">
+            {" "}
+            <NavLink to="/projet"> Voir Plus</NavLink>{" "}
+          </p>
+        </div>
+      </div>
+      <p></p>
+    </div>
+  );
+};
+
+export default ServicesCard;
