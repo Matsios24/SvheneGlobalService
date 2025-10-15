ace903c0a009c55a3e5649cf016e0ddf99fe7f9d
Dieuveille_Matsios <matsionadieuveille@gmail.com>
2025-10-15 21:09:13 +0100

    Finalisation du Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-green-700 text-white text-2xs p-2 font-semibold text-center">
+      <p>Adresse : Base Industrielle, derrière Congo Service</p>
+      <p>Email : contactsgs.net@gmail.com</p>
+      <p>Tel : +242 06 520 32 13 / 05 503 48 22</p>
+      <p>RCCM : N°CG/BZV/11B2292</p>
+      <p>NIU : M2011110000967093</p>
+    </div>
+  );
+};
+
+export default Footer;
