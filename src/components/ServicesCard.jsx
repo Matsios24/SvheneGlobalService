6e55604397d11ac1690c1a3e1935b59ceb79b1d4
Dieuveille_Matsios <matsionadieuveille@gmail.com>
2025-10-17 23:43:51 +0100

    modification du style

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -3,24 +3,31 @@ import { NavLink } from "react-router-dom";
 
 const ServicesCard = ({ title, image, description }) => {
   return (
-    <div className=" w-fit h-6/12 bg-white m-3 rounded-2xl overflow-hidden shadow-2xl shadow-neutral-400">
-      <div className="flex flex-1 flex-col">
+    <div className=" max-sm:min-w-[350px] xl:w-[500px] h-[400px] bg-white m-3 rounded-2xl overflow-hidden shadow-2xl shadow-neutral-400">
+      <div className="grid grid-cols-1 xl:grid-cols-2">
         {/* ------------------------------------------------ */}
         {/* ######### Image */}
         {/* ------------------------------------------------ */}
-
-        <img src={image} alt={title} className="w-[40rem] h-[20rem]" />
+        <div className=" h-full w-full">
+          <img
+            src={image}
+            alt={title}
+            className="w-[40rem] h-[20rem] max-sm:h-full xl:h-[500px] object-cover object-center"
+          />
+        </div>
 
         {/* ------------------------------------------------ */}
         {/* ######### Part title & Description */}
         {/* ------------------------------------------------ */}
 
-        <div className="p-3 flex-1 flex-col">
-          <p className="text-sky-600 text-3xl font-bold text-start">{title}</p>
-          <p className=" overflow-hidden text-gray-800 text-justify text-2xl w-[20rem]">
+        <div className="p-3 flex-1 flex-col max-sm:relative transparent">
+          <p className="text-sky-600 text-3xl font-bold text-start max-sm:bg-transparent">
+            {title}
+          </p>
+          <p className=" overflow-hidden text-gray-800 text-justify text-md w-full">
             {description}
           </p>
-          <p className=" text-end flex justify-center text-xl w-64 font-bold">
+          <p className=" text-end flex justify-center text-xl  font-bold">
             {" "}
             <NavLink to="/projet"> Voir Plus</NavLink>{" "}
           </p>
