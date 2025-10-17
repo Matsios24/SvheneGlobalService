2f411eafb05f3086c06b19f5b68b427eae629de0
Dieuveille_Matsios <matsionadieuveille@gmail.com>
2025-10-17 23:36:56 +0100

    Mise en place et finalisation de la page Service

--- src/pages/ServicePage.jsx
+++ src/pages/ServicePage.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import { services } from "../contents/ContentServices.js";
 import ServicesCard from "../components/ServicesCard";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: { delay: 0.7, duraton: 0.7 },
+    exit: {
+      x: "-100vw",
+      transition: { ease: "easeInOut" },
+    },
+  },
+};
 const ServicePage = () => {
   const { batiment, industrie } = services;
   return (
-    <>
-      <h1 className="text-center font-bold text-5xl underline underline-offset-8 p-5">
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <h2 className="text-center text-6xl font-bold p-5 max-sm:text-4xl text-pink-800 h-auto bg-gradient-to-r from-pink-400  to-sky-500 ">
+        Un service de qualité pour tous vos travaux
+      </h2>
+      <h1 className="text-center text-sky-600 font-bold text-5xl p-5">
         BÂTIMENT
       </h1>
       <div className="flex flex-wrap justify-around w-full h-full items-center mb-3">
@@ -18,20 +41,24 @@ const ServicePage = () => {
           />
         ))}
       </div>
-      <h1 className="text-center font-bold text-5xl underline underline-offset-8 p-5">
+      <h2 className="text-center text-6xl font-bold p-5 max-sm:text-4xl text-pink-800 h-auto bg-gradient-to-r from-pink-400  to-sky-500 ">
+        Un service de qualité pour tous vos travaux
+      </h2>
+      <h1 className="text-center text-sky-600 font-bold text-5xl p-5">
         Industrie
       </h1>
-      <div className="flex flex-wrap justify-around w-full h-full items-center">
+      <div className="flex flex-wrap gap-2 justify-around w-full h-full items-center">
         {industrie.map((items, i) => (
           <ServicesCard
             key={i}
+            transition={{ duration: 0.5, delay: 1 + i * 0.5 }}
             description={items.description}
             image={items.image}
             title={items.title}
           />
         ))}
       </div>
-    </>
+    </motion.div>
   );
 };
 
