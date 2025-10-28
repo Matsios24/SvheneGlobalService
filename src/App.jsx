f66bc8989108f256143379049d700e1893774dbd
Dieuveille_Matsios <matsionadieuveille@gmail.com>
2025-10-28 17:08:27 +0100

    modificaion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
   return (
-    <>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
