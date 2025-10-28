a2ad1d39884b86693e269c90fff4a6092b001907
Dieuveille_Matsios <matsionadieuveille@gmail.com>
2025-10-28 16:57:19 +0100

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-import Aos from "aos";
-import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    Aos.init({
-      duration: 1800,
-      offset: 100,
-      disable: "mobile",
-    });
-  }, []);
   return (
     <>
       <BrowserRouter>
